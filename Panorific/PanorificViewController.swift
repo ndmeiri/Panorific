898da6e203d6176ced5dcf625cf7935afd61dadd
ndmeiri <ndmeiri@users.noreply.github.com>
2014-10-05 14:16:29 -0700

    Update PanorificViewController.swift

--- Panorific/PanorificViewController.swift
+++ Panorific/PanorificViewController.swift
@@ -11,24 +11,31 @@ import CoreMotion
 
 class PanorificViewController: UIViewController, UIScrollViewDelegate {
     
-    var motionManager: CMMotionManager = CMMotionManager()
-    var displayLink: CADisplayLink!
+    // The image that will be diplayed in the receiver.
+    var image: UIImage?
     
-    var panningScrollView: UIScrollView!
-    var panningImageView: UIImageView!
-    var scrollBarView: ImagePanScrollBarView!
-    var motionBasedPanEnabled: Bool = true
+    // Subviews
+    private var panningScrollView: UIScrollView!
+    private var panningImageView: UIImageView!
+    private var scrollBarView: PanorificScrollBarView!
     
-    let MovementSmoothing: CGFloat = 0.3
-    let AnimationDuration: CGFloat = 0.3
-    let RotationMultiplier: CGFloat = 5.0
+    // Managing Motion Sensing
+    private var motionManager: CMMotionManager = CMMotionManager()
+    private var motionBasedPanEnabled: Bool = true
     
-    deinit {
-        self.displayLink.invalidate()
-        self.motionManager.stopDeviceMotionUpdates()
-    }
+    // Managing Animation
+    private var displayLink: CADisplayLink!
+    
+    // Contstants
+    private let MovementSmoothing: CGFloat = 0.3
+    private let AnimationDuration: CGFloat = 0.3
+    private let RotationMultiplier: CGFloat = 5.0
+    
+    // MARK: - UIViewController
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         self.panningScrollView = UIScrollView(frame: self.view.bounds)
         self.panningScrollView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
         self.panningScrollView.backgroundColor = UIColor.blackColor()
@@ -36,7 +43,7 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
         self.panningScrollView.scrollEnabled = false
         self.panningScrollView.alwaysBounceVertical = false
         self.panningScrollView.maximumZoomScale = 2.0
-        self.panningScrollView.pinchGestureRecognizer.addTarget(self, action: Selector("pinchGestureRecognized:"))
+        self.panningScrollView.pinchGestureRecognizer.addTarget(self, action: "pinchGestureRecognized:")
         self.view.addSubview(self.panningScrollView)
         
         self.panningImageView = UIImageView(frame: self.view.bounds)
@@ -45,23 +52,23 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
         self.panningImageView.contentMode = .ScaleAspectFit
         self.panningScrollView.addSubview(self.panningImageView)
         
-        self.scrollBarView = ImagePanScrollBarView(frame: self.view.bounds, edgeInsets: UIEdgeInsetsMake(0.0, 10.0, 50.0, 10.0))
+        self.scrollBarView = PanorificScrollBarView(frame: self.view.bounds, edgeInsets: UIEdgeInsetsMake(0.0, 10.0, 50.0, 10.0))
         self.scrollBarView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
         self.scrollBarView.userInteractionEnabled = false
         self.view.addSubview(self.scrollBarView)
         
-        self.displayLink = CADisplayLink(target: self, selector: Selector("displayLinkUpdate:"))
+        self.displayLink = CADisplayLink(target: self, selector: "displayLinkUpdate:")
         self.displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
         
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("toggleMotionBasedPan:"))
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "toggleMotionBasedPan:")
         self.view.addGestureRecognizer(tapGestureRecognizer)
         
-        self.configureWithImage(UIImage(named: "melbourne"))
-        
-        super.viewDidLoad()
+        self.configureWithImage(self.image!)
     }
     
     override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
         self.panningScrollView.contentOffset = CGPointMake((self.panningScrollView.contentSize.width / 2.0) - (CGRectGetWidth(self.panningScrollView.bounds)) / 2.0,
             (self.panningScrollView.contentSize.height / 2.0) - (CGRectGetHeight(self.panningScrollView.bounds)) / 2.0)
         
@@ -69,17 +76,28 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
             withHandler: { (motion: CMDeviceMotion!, error: NSError!) -> Void in
                 self.calculateRotationBasedOnDeviceMotionRotationRate(motion)
         })
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        self.image = nil
         
-        super.viewDidAppear(animated)
+        super.viewDidDisappear(animated)
+    }
+    
+    deinit {
+        self.displayLink.invalidate()
+        self.motionManager.stopDeviceMotionUpdates()
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-
+    
+    // MARK: - Instance methods
+    
     func configureWithImage(image: UIImage) {
-        self.panningImageView.image = image;
-        self.updateScrollViewZoomToMaximumForImage(image);
+        self.panningImageView.image = image
+        self.updateScrollViewZoomToMaximumForImage(image)
     }
     
     func calculateRotationBasedOnDeviceMotionRotationRate(motion: CMDeviceMotion) {
@@ -88,7 +106,7 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
             let yRotationRate = CGFloat(motion.rotationRate.y)
             let zRotationRate = CGFloat(motion.rotationRate.z)
             
-            if fabs(yRotationRate) > (fabs(xRotationRate) + fabs(zRotationRate)) {
+            if abs(yRotationRate) > (abs(xRotationRate) + abs(zRotationRate)) {
                 let invertedYRotationRate = yRotationRate * -1.0
                 
                 let zoomScale = self.maximumZoomScaleForImage(self.panningImageView.image!)
@@ -107,7 +125,6 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func displayLinkUpdate(displayLink: CADisplayLink) {
-        
         let panningImageViewPresentationLayer: AnyObject! = self.panningImageView.layer.presentationLayer()
         let panningScrollViewPresentationLayer: AnyObject! = self.panningScrollView.layer.presentationLayer()
         
@@ -116,7 +133,7 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
         let contentWidth = CGRectGetWidth(panningImageViewPresentationLayer.frame)
         let visibleWidth = CGRectGetWidth(self.panningScrollView.bounds)
         
-        let clampedXOffsetAsPercentage = fmax(0.0, fmin(1.0, horizontalContentOffset / (contentWidth - visibleWidth)))
+        let clampedXOffsetAsPercentage = max(0.0, min(1.0, horizontalContentOffset / (contentWidth - visibleWidth)))
         
         let scrollBarWidthPercentage = visibleWidth / contentWidth
         let scrollableAreaPercentage = 1.0 - scrollBarWidthPercentage
@@ -127,7 +144,6 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func toggleMotionBasedPan(sender: AnyObject) {
-        
         let motionBasedPanWasEnabled = self.motionBasedPanEnabled
         
         if motionBasedPanWasEnabled {
@@ -135,8 +151,8 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
         }
         
         UIView.animateWithDuration(NSTimeInterval(AnimationDuration), animations: {
-                self.updateViewsForMotionBasedPanEnabled(!motionBasedPanWasEnabled)
-            }, completion: { (value: Bool) in
+            self.updateViewsForMotionBasedPanEnabled(!motionBasedPanWasEnabled)
+            }, completion: { (_) in
                 if !motionBasedPanWasEnabled {
                     self.motionBasedPanEnabled = true
                 }
@@ -148,8 +164,8 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
             self.updateScrollViewZoomToMaximumForImage(self.panningImageView.image!)
             self.panningScrollView.scrollEnabled = false
         } else {
-            self.panningScrollView.zoomScale = 1.0;
-            self.panningScrollView.scrollEnabled = true;
+            self.panningScrollView.zoomScale = 1.0
+            self.panningScrollView.scrollEnabled = true
         }
     }
     
@@ -168,16 +184,18 @@ class PanorificViewController: UIViewController, UIScrollViewDelegate {
         let maximumXOffset = self.panningScrollView.contentSize.width - CGRectGetWidth(self.panningScrollView.bounds)
         let minimumXOffset: CGFloat = 0.0
         
-        let clampedXOffset = fmax(minimumXOffset, fmin(horizontalOffset, maximumXOffset))
+        let clampedXOffset = max(minimumXOffset, min(horizontalOffset, maximumXOffset))
         let centeredY = (self.panningScrollView.contentSize.height / 2.0) - (CGRectGetHeight(self.panningScrollView.bounds)) / 2.0
         
         return CGPointMake(clampedXOffset, centeredY)
     }
     
     func pinchGestureRecognized(sender: AnyObject) {
-        self.motionBasedPanEnabled = false;
-        self.panningScrollView.scrollEnabled = true;
+        self.motionBasedPanEnabled = false
+        self.panningScrollView.scrollEnabled = true
     }
+    
+    // MARK: - UIScrollViewDelegate
     
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
         return self.panningImageView
